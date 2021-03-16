e04a114b3feb60fad322e8f3cb67b0ac29c81744
Mireag Madalina <mireagmada@gmail.com>
2021-03-16 13:50:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bookstory/Carte.java
@@ -0,0 +1,9 @@
+package com.example.bookstory;
+
+public class Carte {
+    private int id;
+    private String titlu;
+    private String ISBN;
+    private int nrCopiiDisponibile;
+
+}
---
+++ app/src/main/java/com/example/bookstory/Utilizator.java
@@ -0,0 +1,10 @@
+package com.example.bookstory;
+
+public class Utilizator {
+    private int id;
+    private String Nume;
+    private String Prenume;
+    private String Email;
+    private String Password;
+    
+}
