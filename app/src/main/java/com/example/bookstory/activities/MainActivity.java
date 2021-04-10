67c9ee32df2e4db99547b5a01622c31966695746
Mireag Madalina <mireagmada@gmail.com>
2021-04-10 18:02:16 +0300

    started working on email and password authentication

--- app/src/main/java/com/example/bookstory/activities/MainActivity.java
+++ app/src/main/java/com/example/bookstory/activities/MainActivity.java
@@ -26,30 +26,30 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),ListareCartiActivity.class);
+                Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
                 startActivity(intent);
-                Autor autor = new Autor("Mark Manson");
-                Autor autor1 = new Autor("John Green");
-                Autor autor2 = new Autor("alt autor");
-
-                Carte carte = new Carte("Arta Subtila a disperarii","345-3h4g-45r", Gen.FICTION,3,"ceva random");
-                Carte carte1 = new Carte("Fulgi de iubire","444-3h45g-45r", Gen.FICTION,3,"ceva random");
-                LibraryDB db = LibraryDB.getInstanta(getApplicationContext());
-                db.getAutorDao().deleteAll();
-                db.getCartiDao().deleteAll();
-                db.getCarteDao().deleteAllFromJoinTable();
-                autor.setIdAutor(db.getAutorDao().insert(autor));
-                autor1.setIdAutor(db.getAutorDao().insert(autor1));
-                autor2.setIdAutor(db.getAutorDao().insert(autor2));
-
-                carte.setId((int)db.getCartiDao().insert(carte));
-                carte1.setId((int)db.getCartiDao().insert(carte1));
-                AutorCarte autorCarte = new AutorCarte(autor.getIdAutor(),carte.getIdCarte());
-                AutorCarte autorCarte1 = new AutorCarte(autor1.getIdAutor(),carte1.getIdCarte());
-                AutorCarte autorCarte2 = new AutorCarte(autor2.getIdAutor(),carte1.getIdCarte());
-                db.getCarteDao().insert(autorCarte);
-                db.getCarteDao().insert(autorCarte1);
-                db.getCarteDao().insert(autorCarte2);
+//                Autor autor = new Autor("Mark Manson");
+//                Autor autor1 = new Autor("John Green");
+//                Autor autor2 = new Autor("alt autor");
+//
+//                Carte carte = new Carte("Arta Subtila a disperarii","345-3h4g-45r", Gen.FICTION,3,"ceva random");
+//                Carte carte1 = new Carte("Fulgi de iubire","444-3h45g-45r", Gen.FICTION,3,"ceva random");
+//                LibraryDB db = LibraryDB.getInstanta(getApplicationContext());
+//                db.getAutorDao().deleteAll();
+//                db.getCartiDao().deleteAll();
+//                db.getCarteDao().deleteAllFromJoinTable();
+//                autor.setIdAutor(db.getAutorDao().insert(autor));
+//                autor1.setIdAutor(db.getAutorDao().insert(autor1));
+//                autor2.setIdAutor(db.getAutorDao().insert(autor2));
+//
+//                carte.setId((int)db.getCartiDao().insert(carte));
+//                carte1.setId((int)db.getCartiDao().insert(carte1));
+//                AutorCarte autorCarte = new AutorCarte(autor.getIdAutor(),carte.getIdCarte());
+//                AutorCarte autorCarte1 = new AutorCarte(autor1.getIdAutor(),carte1.getIdCarte());
+//                AutorCarte autorCarte2 = new AutorCarte(autor2.getIdAutor(),carte1.getIdCarte());
+//                db.getCarteDao().insert(autorCarte);
+//                db.getCarteDao().insert(autorCarte1);
+//                db.getCarteDao().insert(autorCarte2);
             }
         });
     }
--- app/src/main/java/com/example/bookstory/activities/RegisterActivity.java
+++ app/src/main/java/com/example/bookstory/activities/RegisterActivity.java
@@ -1,16 +1,88 @@
 package com.example.bookstory.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.bookstory.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText etNume;
+    EditText etPrenume;
+    EditText etEmail;
+    EditText etPassword;
+    Button btnRegister;
+    FirebaseAuth auth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        etNume = findViewById(R.id.etNume);
+        etEmail = findViewById(R.id.etEmail);
+        btnRegister = findViewById(R.id.btnRegisterDo);
+        etPassword = findViewById(R.id.etPassword);
+        progressBar=findViewById(R.id.progressBarRegister);
+        auth = FirebaseAuth.getInstance();
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerNewUser();
+            }
+        });
+    }
+    private void registerNewUser() {
+        progressBar.setVisibility(View.VISIBLE);
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(getApplicationContext(), R.string.enter_email, Toast.LENGTH_LONG).show();
+            progressBar.setVisibility(View.GONE);
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(getApplicationContext(), R.string.enter_password, Toast.LENGTH_LONG).show();
+            progressBar.setVisibility(View.GONE);
+            return;
+        }
+        if(password.length() < 6){
+            Toast.makeText(getApplicationContext(), R.string.invalid_password, Toast.LENGTH_LONG).show();
+        }
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), R.string.reg_successfull, Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), R.string.reg_failed, Toast.LENGTH_LONG).show();
+                    Log.i("EROARE",task.getResult().toString());
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        }).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName(etNume.getText().toString()).build();
+                authResult.getUser().updateProfile(profileUpdates);
+            }
+        });
+
     }
 }
